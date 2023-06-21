293db5bd3c2dd7b1c16a888d88bd96ac548dee63
master <spikerus93@rambler.ru>
2023-06-22 01:32:58 +0300

    Решение задач на 4-ом Семинаре

---
+++ task26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt()
+{
+    Console.WriteLine("Введите Любое Число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int func(int number)
+{
+    int col = 0;
+    while(number > 0)
+    {
+        number = number / 10;
+        col++;
+    }
+    return col;
+}
+
+int number = ReadInt();
+int col = func(number);
+Console.WriteLine($"Количество цифр в Вашем числе: {col} ");
+
+
---
+++ task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt()
+{
+    Console.WriteLine("Введите Любое Число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Pro(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int number = ReadInt();
+int Pro1 = Pro(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {Pro1}");
